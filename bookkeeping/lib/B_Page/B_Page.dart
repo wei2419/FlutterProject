e538859b879d7ce1f6a70fa68e69de87cf5d2006
wei2419 <forever.time75@gmail.com>
2021-08-14 22:16:39 +0800

    test TabBar
    在B Page 增加了TabBar功能

--- bookkeeping/lib/B_Page/B_Page.dart
+++ bookkeeping/lib/B_Page/B_Page.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:bookkeeping/main.dart';
 
 class BPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('我是 B 頁'),
+    return DefaultTabController(
+      length: 2,
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              onTap: (index) {
+                // Tab index when user select it, it start from zero
+              },
+              tabs: [
+                Tab(icon: Icon(Icons.card_travel)),
+                Tab(icon: Icon(Icons.add_shopping_cart)),
+              ],
+            ),
+            title: Text('Tabs Demo'),
+          ),
+          body: TabBarView(
+            children: [
+              Center(
+                  child: Text(
+                    "0",
+                    style: TextStyle(fontSize: 40),
+                  )),
+              Center(
+                  child: _BPage()),
+            ],
+          ),
+        ),
       ),
-      body: _BPage(),
     );
   }
 }
@@ -20,7 +45,7 @@ class _BPage extends StatelessWidget {
       child: ElevatedButton(
         child: Text('返回首頁'),
         onPressed: () {
-          Navigator.pop(context);
+          Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp()));
         },
       ),
     );
